0fb1f5fdece5615552aed42332e350ac24287407
cid02219000 <147800186+jiahuili12@users.noreply.github.com>
2024-12-05 17:54:41 +0800

    Update main.s

--- main.s
+++ main.s
@@ -1,24 +1,47 @@
-	#include <xc.inc>
+#include <xc.inc>
 
-psect	code, abs
-	
-main:
-	org	0x1
-	goto	start
+extrn	sensor_setup, sensor_send_signal, sensor_distance
+extrn	motor_setup, Forward, Backward, Left, Right, Stop, motor_test
 
-	org	0x100		    ; Main code starts here at address 0x100
-start:
-	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
-	bra 	test
-loop:
-	movff 	0x06, PORTB
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+global	safety_dist
+    
+psect	udata_acs
+safety_dist:	ds 1			; Reserve 1 bit for safety distance
 
-	end	main
+psect code, abs
+ 
+rst:
+    org 0x0
+    goto setup
+
+ 
+setup:
+    call motor_setup			    ; Initialize motor setup
+    call sensor_setup			    ; Initialize sensor setup
+    call CCP_setup			    ; Initialize CCP module
+    
+start:    
+    movlw	0x0A			    
+    movwf	safety_dist, A
+    
+main_loop:
+    call	sensor_send_signal	    ; Send ultrasonic pulse
+    call	CCP_reset		    ; Reset CCP and Timer
+    call	CCP_setup		    ; Re-enable CCP and Timer
+
+    call	sensor_distance		    ; Check distance
+    btfss	STATUS, 2, A		    ; Check if Z flag is set (safe distance), status register bit 2 is z = zero bit. 
+					    ; if Z=0(distance<safety distance), jumps to emergency_stop
+    goto	emergency_stop
+
+    goto	continue_vehicle
+
+    
+emergency_stop:
+    call	Stop			    ; Stop the vehicle
+
+continue_vehicle:
+    call	motor_test
+    goto	main_loop
+
+end rst
